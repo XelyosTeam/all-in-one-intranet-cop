d6a637ea450533cb6fef54320a8552546ee5f667
Aros <49004550+Tvpirates@users.noreply.github.com>
2021-08-17 00:39:28 +0200

    v3.1.0

--- entity/candidature.php
+++ entity/candidature.php
@@ -1,11 +1,12 @@
 <?php
 /*
-Le projet All in One est un produit Xelyos mis à disposition gratuitement
-pour tous les serveurs de jeux Role Play. En échange nous vous demandons de
-ne pas supprimer le ou les auteurs du projet.
-Created by : Xelyos - Aros
-Edited by :
+  Le projet All in One est un produit Xelyos mis à disposition gratuitement
+  pour tous les serveurs de jeux Role Play. En échange nous vous demandons de
+  ne pas supprimer le ou les auteurs du projet.
+  Created by : Xelyos - Aros
+  Edited by :
 */
+
 use Josantonius\Session\Session; // Pour utiliser les variables de sessions
 
 class Candidature extends Model {
@@ -13,13 +14,11 @@ class Candidature extends Model {
 
   /* Récupération des valeurs dans les tables */
   public static function getListCandidature($etat) { // On récupère la liste du casier judiciaire avec l'ID de la personne
-    return Candidature::where('etat_act', $etat)
-                      ->order_by_asc(array('nom', 'prenom'))
+    return Candidature::where(array('etat_act' => $etat, 'version' => serveurIni('Parametre', 'version_candidature')))
                       ->find_many();
   }
-  
   public static function getCandidature($id_candid) { // On récupère la liste du casier judiciaire avec l'ID de la personne
-    return Candidature::where('id', $id_candid)
+    return Candidature::where(array('id' => $id_candid, 'version' => serveurIni('Parametre', 'version_candidature')))
                       ->find_one();
   }
 }
